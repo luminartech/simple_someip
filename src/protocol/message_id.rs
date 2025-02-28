386e2f13fef19e3347cb52d2afd30b83c127eb37
Zach <zheylmun@gmail.com>
2025-02-28 10:52:43 -0500

    Add const to message

--- src/protocol/message_id.rs
+++ src/protocol/message_id.rs
@@ -17,57 +17,58 @@ impl From<u32> for MessageId {
 
 impl MessageId {
     /// Create a new MessageId.
-    pub fn new(message_id: u32) -> Self {
+    pub const fn new(message_id: u32) -> Self {
         MessageId(message_id)
     }
-    pub fn new_sd() -> Self {
+    pub const fn new_sd() -> Self {
         MessageId(SD_MESSAGE_ID)
     }
 
     /// Get the message ID
     #[inline]
-    pub fn message_id(&self) -> u32 {
+    pub const fn message_id(&self) -> u32 {
         self.0
     }
 
     /// Set the message ID
     #[inline]
-    pub fn set_message_id(&mut self, message_id: u32) {
+    pub const fn set_message_id(&mut self, message_id: u32) {
         self.0 = message_id;
     }
 
     /// Get the service ID
     #[inline]
-    pub fn service_id(&self) -> u16 {
+    pub const fn service_id(&self) -> u16 {
         (self.0 >> 16) as u16
     }
 
     /// Set the service ID
     #[inline]
-    pub fn set_service_id(&mut self, service_id: u16) {
+    pub const fn set_service_id(&mut self, service_id: u16) {
         self.0 = (self.0 & 0xFFFF) | ((service_id as u32) << 16);
     }
 
     /// Get the method ID
     #[inline]
-    pub fn method_id(&self) -> u16 {
+    pub const fn method_id(&self) -> u16 {
         (self.0 & 0xFFFF) as u16
     }
 
     /// Set the method ID
     #[inline]
-    pub fn set_method_id(&mut self, method_id: u16) {
+    pub const fn set_method_id(&mut self, method_id: u16) {
         self.0 = (self.0 & 0xFFFF0000) | method_id as u32;
     }
 
     /// Message is Event/Notification
     #[inline]
-    pub fn is_event(&self) -> bool {
+    pub const fn is_event(&self) -> bool {
         self.method_id() & 0x8000 != 0
     }
 
     /// Message is SOME/IP Service Discovery
-    pub fn is_sd(&self) -> bool {
+    #[inline]
+    pub const fn is_sd(&self) -> bool {
         self.0 == SD_MESSAGE_ID
     }
 }
