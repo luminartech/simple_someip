a163b98950edc7dc2be83e1ab72cf9ef781092f8
Zach <zheylmun@gmail.com>
2025-03-20 17:07:53 -0400

    check in work for @kim

--- src/client/inner.rs
+++ src/client/inner.rs
@@ -20,21 +20,33 @@ use crate::{
 
 use super::{ClientUpdate, DiscoveryInfo};
 
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub(super) enum Control {
     SetInterface(Ipv4Addr),
     BindDiscovery,
     UnbindDiscovery,
+    BindUnicast,
+    UnbindUnicast,
 }
 
+#[derive(Debug)]
 pub(super) struct ControlMessage {
     control: Control,
+    step: u8,
     response: oneshot::Sender<Result<(), Error>>,
 }
 
 impl ControlMessage {
     pub fn new(control: Control) -> (Self, oneshot::Receiver<Result<(), Error>>) {
         let (response, receiver) = oneshot::channel();
-        (Self { control, response }, receiver)
+        (
+            Self {
+                control,
+                step: 0,
+                response,
+            },
+            receiver,
+        )
     }
 }
 
@@ -46,12 +58,21 @@ struct UnicastInfo<MessageDefinitions> {
 
 #[derive(Debug)]
 pub(super) struct Inner<MessageDefinitions> {
+    /// MPSC Receiver used to receive control messages from outer client
     control_receiver: Receiver<ControlMessage>,
+    /// The active request, if one is being served
+    active_request: Option<ControlMessage>,
+    /// MPSC Sender used to send updates to outer client
     update_sender: mpsc::Sender<ClientUpdate<MessageDefinitions>>,
+    /// Target interface for sockets
     interface: Ipv4Addr,
+    /// MPSC Receiver used to receive discovery messages
     discovery_receiver: Option<mpsc::Receiver<Result<Message<MessageDefinitions>, Error>>>,
+    /// Discovery information containing endpoint information
     discovery_info: DiscoveryInfo,
+    /// Unicast information containing the receiver and port if a unicase socket is bound
     unicast_info: Option<UnicastInfo<MessageDefinitions>>,
+    /// Phantom data to represent the generic message definitions
     phantom: std::marker::PhantomData<MessageDefinitions>,
 }
 
@@ -69,6 +90,7 @@ where
         let (update_sender, update_receiver) = mpsc::channel(16);
         let inner = Self {
             control_receiver,
+            active_request: None,
             update_sender,
             interface,
             discovery_receiver: None,
@@ -110,7 +132,11 @@ where
         self.discovery_receiver = None;
     }
 
-    async fn bind_unicast_to_interface(&mut self, interface: Ipv4Addr) -> Result<(), Error> {
+    async fn set_interface(&mut self, interface: Ipv4Addr) {
+        self.interface = interface;
+    }
+
+    async fn bind_unicast(&mut self, interface: Ipv4Addr) -> Result<(), Error> {
         let (sender, receiver) = mpsc::channel(16);
         let bind_addr = SocketAddr::new(IpAddr::V4(interface), 0);
         let unicast_socket = UdpSocket::bind(bind_addr).await?;
@@ -152,6 +178,11 @@ where
         Ok(())
     }
 
+    async fn unbind_unicast(&mut self) {
+        self.unicast_info = None;
+        tokio::time::sleep(std::time::Duration::from_millis(250)).await;
+    }
+
     async fn receive_discovery(
         discovery_receiver: &mut Option<mpsc::Receiver<Result<Message<MessageDefinitions>, Error>>>,
     ) -> Result<sd::Header, Error> {
@@ -176,6 +207,7 @@ where
             future::pending().await
         }
     }
+
     /*
         async fn receive_unicast(&mut self) -> Result<MessageDefinitions, Error> {
             if let Some(unicast_info) = self.unicast_info.as_ref() {
@@ -185,6 +217,67 @@ where
             }
         }
     */
+
+    async fn handle_control_message(&mut self, control: ControlMessage) {
+        if let Some(ControlMessage {
+            control,
+            mut step,
+            response,
+        }) = self.active_request
+        {
+            match control {
+                Control::SetInterface(interface) => {
+                    if step == 0 {
+                        if response.send(Ok(())).is_err() {
+                            // The sender has been dropped, so we should exit
+                            return;
+                        }
+                        step += 1;
+                    }
+                }
+                Control::BindDiscovery => {
+                    if step == 0 {
+                        if response.send(self.bind_discovery().await).is_err() {
+                            // The sender has been dropped, so we should exit
+                            return;
+                        }
+                        step += 1;
+                    }
+                }
+                Control::UnbindDiscovery => {
+                    if step == 0 {
+                        self.unbind_discovery();
+                        if response.send(Ok(())).is_err() {
+                            // The sender has been dropped, so we should exit
+                            return;
+                        }
+                    }
+                }
+                Control::BindUnicast => {
+                    if step == 0 {
+                        if response
+                            .send(self.bind_unicast(self.interface).await)
+                            .is_err()
+                        {
+                            // The sender has been dropped, so we should exit
+                            return;
+                        }
+                        step += 1;
+                    }
+                }
+                Control::UnbindUnicast => {
+                    if step == 0 {
+                        self.unbind_unicast().await;
+                        if response.send(Ok(())).is_err() {
+                            // The sender has been dropped, so we should exit
+                            return;
+                        }
+                        step += 1;
+                    }
+                }
+            }
+        }
+    }
     fn run(mut self) {
         tokio::spawn(async move {
             loop {
@@ -196,41 +289,39 @@ where
                     ..
                 } = &mut self;
                 select! {
-
+                    // Receive a control message
                     ctrl = control_receiver.recv() => {
-                        if let Some( ControlMessage{ control, response }) = ctrl {
-                            match control {
-                                Control::SetInterface(interface) => {
-                                    self.interface = interface;
-                                    response.send(Ok(())).unwrap();
-                                }
-                                Control::BindDiscovery => {
-                                    response.send(self.bind_discovery().await).unwrap();
-                                }
-                                Control::UnbindDiscovery => {
-                                    self.unbind_discovery();
-                                    response.send(Ok(())).unwrap();
-                                }
-                            }
+                        if let Some(ctrl) = ctrl {
+                            self.active_request = Some(ctrl);
                         } else {
                             // The sender has been dropped, so we should exit
                             break;
                         }
                     }
+                    // Receive a discovery message
                     discovery = Inner::receive_discovery(discovery_receiver) => {
                         match discovery {
                             Ok(header) => {
                                 match discovery_info.update(header) {
                                     Ok(info) => {
-                                        update_sender.send(ClientUpdate::DiscoveryUpdated(info)).await.unwrap();
+                                        if update_sender.send(ClientUpdate::DiscoveryUpdated(info)).await.is_err() {
+                                            // The sender has been dropped, so we should exit
+                                            break;
+                                        }
                                     }
                                     Err(err) => {
-                                        update_sender.send(ClientUpdate::Error(err)).await.unwrap();
+                                        if update_sender.send(ClientUpdate::Error(err)).await.is_err() {
+                                            // The sender has been dropped, so we should exit
+                                            break;
+                                        }
                                     }
                                 }
                             }
                             Err(err) => {
-                                update_sender.send(ClientUpdate::Error(err)).await.unwrap();
+                                if update_sender.send(ClientUpdate::Error(err)).await.is_err() {
+                                    // The sender has been dropped, so we should exit
+                                    break;
+                                }
                             }
                         }
                      }
