4e8b02c6dbb04821fcdd65f7b8492c2f53059038
Zach <zheylmun@gmail.com>
2025-04-04 10:52:35 -0400

    allow for per-message addressing

--- src/error.rs
+++ src/error.rs
@@ -5,9 +5,13 @@ pub enum Error {
     #[error(transparent)]
     ProtocolError(#[from] crate::protocol::Error),
     #[error(transparent)]
-    Io(#[from] std::io::Error),
+    Io(#[from] tokio::io::Error),
     #[error("Unexpected discovery message: {0:?}")]
     UnexpectedDiscoveryMessage(crate::protocol::Header),
     #[error("Invalid SD Header: {0:?}")]
     InvalidSDHeader(crate::protocol::sd::Header),
+    #[error("Socket Closed Unexpectedly")]
+    SocketClosedUnexpectedly,
+    #[error("Unicast Socket not bound")]
+    UnicastSocketNotBound,
 }
