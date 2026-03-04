dc80e494fb4cac63b8b0e8b6f1e24305056402e7
Zach Heylmun <zheylmun@gmail.com>
2026-03-04 12:32:58 -0500

    Document client/*.rs

--- src/client/error.rs
+++ src/client/error.rs
@@ -1,17 +1,24 @@
 use thiserror::Error;
 
+/// Errors that can occur during SOME/IP client operations.
 #[derive(Error, Debug)]
 pub enum Error {
+    /// A SOME/IP protocol-level error.
     #[error(transparent)]
     Protocol(#[from] crate::protocol::Error),
+    /// An I/O error from the underlying network transport.
     #[error(transparent)]
     Io(#[from] std::io::Error),
+    /// Received a discovery message that was not expected.
     #[error("Unexpected discovery message: {0:?}")]
     UnexpectedDiscoveryMessage(crate::protocol::Header),
+    /// A socket was closed unexpectedly.
     #[error("Socket Closed Unexpectedly")]
     SocketClosedUnexpectedly,
+    /// The unicast socket has not been bound yet.
     #[error("Unicast Socket not bound")]
     UnicastSocketNotBound,
+    /// The requested service was not found in the endpoint registry.
     #[error("Service not found in endpoint registry")]
     ServiceNotFound,
 }
